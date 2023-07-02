74419ab3bede72d8e2e60d7fba690f015f1c82d8
djelavic_ag <djelavic_ag@medable.com>
2023-07-02 10:25:09 +0200

    Update mongoose, add missing cors, add env var example

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const expressValidator = require("express-validator");
-require("dotenv").config();
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const expressValidator = require('express-validator')
+require('dotenv')
+  .config()
 // import routes
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const categoryRoutes = require("./routes/category");
-const productRoutes = require("./routes/product");
-const braintreeRoutes = require("./routes/braintree");
-const orderRoutes = require("./routes/order");
+const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
+const categoryRoutes = require('./routes/category')
+const productRoutes = require('./routes/product')
+const braintreeRoutes = require('./routes/braintree')
+const orderRoutes = require('./routes/order')
 
 // app
-const app = express();
+const app = express()
 
 // db
 mongoose
@@ -23,26 +24,27 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true
   })
-  .then(() => console.log("DB Connected"));
+  .then(() => console.log('DB Connected'))
 
 // middlewares
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(expressValidator());
-app.use(cors());
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(expressValidator())
+app.use(cors())
 
 // routes middleware
-app.use("/api", authRoutes);
-app.use("/api", userRoutes);
-app.use("/api", categoryRoutes);
-app.use("/api", productRoutes);
-app.use("/api", braintreeRoutes);
-app.use("/api", orderRoutes);
+app.use('/api', authRoutes)
+app.use('/api', userRoutes)
+app.use('/api', categoryRoutes)
+app.use('/api', productRoutes)
+app.use('/api', braintreeRoutes)
+app.use('/api', orderRoutes)
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
