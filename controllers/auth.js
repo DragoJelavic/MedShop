75ef690e9abf651d8bbd6720221e75918d5d0eb3
djelavic_ag <djelavic_ag@medable.com>
2023-07-03 15:32:50 +0200

    Refactor auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,51 +6,55 @@ const { expressjwt: expressJwt } = require('express-jwt'); // for authorization 
 const User = require('../models/user');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
-exports.signup = (req, res) => {
-  const newUser = new User(req.body);
-  newUser.save((err, user) => {
-    if (err) {
-      return res.status(400)
-        .json({
-          error: errorHandler(err),
-        });
-    }
+exports.signup = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const user = await newUser.save();
     user.salt = undefined;
     user.hashed_password = undefined;
-    return res.json({
-      user,
-    });
-  });
+    res.json({ user });
+  } catch (err) {
+    res.status(400).json({ error: errorHandler(err) });
+  }
 };
 
-exports.signin = (req, res) => {
-  const { email, password } = req.body;
-  User.findOne({ email }, (err, user) => {
-    if (err || !user) {
-      return res.status(400)
-        .json({
-          error: 'User with that email does not exist. Please signup',
-        });
+exports.signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    const expiresInDays = 7;
+    const expirationMs = expiresInDays * 24 * 60 * 60 * 1000;
+    const expirationDate = new Date().getTime() + expirationMs;
+
+    if (!user) {
+      return res.status(400).json({
+        error: 'User with that email does not exist. Please sign up.',
+      });
     }
+
     if (!user.authenticate(password)) {
-      return res.status(401)
-        .json({
-          error: 'Email and password dont match',
-        });
+      return res.status(401).json({
+        error: 'Email and password do not match.',
+      });
     }
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-    res.cookie('t', token, { expire: new Date() + 9999 });
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: expiresInDays });
+
+    res.cookie('t', token, { expire: new Date(expirationDate) });
+
     const {
-      // eslint-disable-next-line no-shadow
-      _id, name, email, role,
+      _id, name, email: userEmail, role,
     } = user;
+
     return res.json({
       token,
       user: {
-        _id, email, name, role,
+        _id, userEmail, name, role,
       },
     });
-  });
+  } catch (err) {
+    res.status(400).json({ error: errorHandler(err) });
+  }
 };
 
 exports.signout = (req, res) => {
