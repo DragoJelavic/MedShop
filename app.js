29b6878c274105646204a2493fed4cc7466c9421
djelavic_ag <djelavic_ag@medable.com>
2023-07-03 09:33:20 +0200

    Remove expressValidator reference

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-const expressValidator = require('express-validator')
 require('dotenv')
   .config()
 // import routes
