14eefea6bee2996403c7461a2d74d7112c5d55c1
djelavic_ag <djelavic_ag@medable.com>
2023-07-03 09:24:43 +0200

    Mongoose major upgrade

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ const app = express()
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
     useUnifiedTopology: true
   })
   .then(() => console.log('DB Connected'))
