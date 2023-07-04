40889d959bdca0a32993a3f1681d699fc16e4b57
djelavic_ag <djelavic_ag@medable.com>
2023-07-04 10:36:47 +0200

    Move product controller functions to async

--- controllers/product.js
+++ controllers/product.js
@@ -6,72 +6,73 @@ const fs = require('fs');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
-exports.productById = (req, res, next, id) => {
-  Product.findById(id)
-    .populate('category')
-    .exec((err, product) => {
-      if (err || !product) {
-        return res.status(400).json({
-          error: 'Product not found',
-        });
-      }
-      req.product = product;
-      next();
+exports.productById = async (req, res, next, id) => {
+  try {
+    const product = await Product.findById(id).populate('category');
+    if (!product) {
+      return res.status(400).json({
+        error: 'Product not found',
+      });
+    }
+    req.product = product;
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      error: 'Internal server error',
     });
+  }
 };
 
 exports.read = (req, res) => {
   req.product.photo = undefined;
-  return res.json(req.product);
+  res.json(req.product);
 };
 
 exports.create = (req, res) => {
   const form = new formidable.IncomingForm();
   form.keepExtensions = true;
-  form.parse(req, (err, fields, files) => {
+  form.parse(req, async (err, fields, files) => {
     if (err) {
       return res.status(400).json({
         error: 'Image could not be uploaded',
       });
     }
-    // check for all fields
-    const {
-      name, description, price, category, quantity, shipping,
-    } = fields;
 
-    if (
-      !name
-      || !description
-      || !price
-      || !category
-      || !quantity
-      || !shipping
-    ) {
-      return res.status(400).json({
-        error: 'All fields are required',
+    try {
+      const { name, description, price, category, quantity, shipping } = fields;
+
+      if (
+        !name ||
+        !description ||
+        !price ||
+        !category ||
+        !quantity ||
+        !shipping
+      ) {
+        return res.status(400).json({
+          error: 'All fields are required',
+        });
+      }
+
+      const product = new Product(fields);
+
+      if (files.photo) {
+        if (files.photo.size > 1000000) {
+          return res.status(400).json({
+            error: 'Image should be less than 1mb in size',
+          });
+        }
+        product.photo.data = fs.readFileSync(files.photo.path);
+        product.photo.contentType = files.photo.type;
+      }
+
+      const savedProduct = await product.save();
+      res.json(savedProduct);
+    } catch (error) {
+      return res.status(500).json({
+        error: 'Internal server error',
       });
     }
-
-    const product = new Product(fields);
-
-    if (files.photo) {
-      if (files.photo.size > 1000000) {
-        return res.status(400).json({
-          error: 'Image should be less than 1mb in size',
-        });
-      }
-      product.photo.data = fs.readFileSync(files.photo.path);
-      product.photo.contentType = files.photo.type;
-    }
-
-    product.save((error, result) => {
-      if (error) {
-        return res.status(400).json({
-          error: errorHandler(error),
-        });
-      }
-      res.json(result);
-    });
   });
 };
 
@@ -93,121 +94,125 @@ exports.remove = (req, res) => {
 exports.update = (req, res) => {
   const form = new formidable.IncomingForm();
   form.keepExtensions = true;
-  form.parse(req, (err, fields, files) => {
+  form.parse(req, async (err, fields, files) => {
     if (err) {
       return res.status(400).json({
         error: 'Image could not be uploaded',
       });
     }
 
-    let { product } = req;
-    product = _.extend(product, fields);
+    try {
+      let { product } = req;
+      product = _.extend(product, fields);
 
-    if (files.photo) {
-      if (files.photo.size > 1000000) {
-        return res.status(400).json({
-          error: 'Image should be less than 1mb in size',
-        });
+      if (files.photo) {
+        if (files.photo.size > 1000000) {
+          return res.status(400).json({
+            error: 'Image should be less than 1mb in size',
+          });
+        }
+        product.photo.data = fs.readFileSync(files.photo.path);
+        product.photo.contentType = files.photo.type;
       }
-      product.photo.data = fs.readFileSync(files.photo.path);
-      product.photo.contentType = files.photo.type;
+
+      const updatedProduct = await product.save();
+      res.json(updatedProduct);
+    } catch (error) {
+      return res.status(500).json({
+        error: 'Internal server error',
+      });
     }
-
-    product.save((error, result) => {
-      if (error) {
-        return res.status(400).json({
-          error: errorHandler(error),
-        });
-      }
-      res.json(result);
-    });
   });
 };
 
-exports.list = (req, res) => {
-  const order = req.query.order ? req.query.order : 'asc';
-  const sortBy = req.query.sortBy ? req.query.sortBy : '_id';
-  const limit = req.query.limit ? parseInt(req.query.limit, 10) : 6;
+exports.list = async (req, res) => {
+  try {
+    const order = req.query.order ? req.query.order : 'asc';
+    const sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+    const limit = req.query.limit ? parseInt(req.query.limit, 10) : 6;
 
-  Product.find()
-    .select('-photo')
-    .populate('category')
-    .sort([[sortBy, order]])
-    .limit(limit)
-    .exec((err, products) => {
-      if (err) {
-        return res.status(400).json({
-          error: 'Products not found',
-        });
-      }
-      res.json(products);
+    const products = await Product.find()
+      .select('-photo')
+      .populate('category')
+      .sort([[sortBy, order]])
+      .limit(limit);
+
+    res.json(products);
+  } catch (error) {
+    return res.status(500).json({
+      error: 'Internal server error',
     });
+  }
 };
 
-exports.listRelated = (req, res) => {
-  const limit = req.query.limit ? parseInt(req.query.limit, 10) : 6;
+exports.listRelated = async (req, res) => {
+  try {
+    const limit = req.query.limit ? parseInt(req.query.limit, 10) : 6;
 
-  Product.find({ _id: { $ne: req.product }, category: req.product.category })
-    .limit(limit)
-    .populate('category', '_id name')
-    .exec((err, products) => {
-      if (err) {
-        return res.status(400).json({
-          error: 'Products not found',
-        });
-      }
-      res.json(products);
+    const products = await Product.find({
+      _id: { $ne: req.product },
+      category: req.product.category,
+    })
+      .limit(limit)
+      .populate('category', '_id name');
+
+    res.json(products);
+  } catch (error) {
+    return res.status(500).json({
+      error: 'Internal server error',
     });
+  }
 };
 
-exports.listCategories = (req, res) => {
-  Product.distinct('category', {}, (err, categories) => {
-    if (err) {
-      return res.status(400).json({
-        error: 'Categories not found',
-      });
-    }
+exports.listCategories = async (req, res) => {
+  try {
+    const categories = await Product.distinct('category', {});
+
     res.json(categories);
-  });
+  } catch (error) {
+    return res.status(500).json({
+      error: 'Internal server error',
+    });
+  }
 };
 
-exports.listBySearch = (req, res) => {
-  const order = req.body.order ? req.body.order : 'desc';
-  const sortBy = req.body.sortBy ? req.body.sortBy : '_id';
-  const limit = req.body.limit ? parseInt(req.body.limit, 10) : 100;
-  const skip = parseInt(req.body.skip, 10);
-  const findArgs = {};
+exports.listBySearch = async (req, res) => {
+  try {
+    const order = req.body.order ? req.body.order : 'desc';
+    const sortBy = req.body.sortBy ? req.body.sortBy : '_id';
+    const limit = req.body.limit ? parseInt(req.body.limit, 10) : 100;
+    const skip = parseInt(req.body.skip, 10);
+    const findArgs = {};
 
-  Object.entries(req.body.filters).forEach(([key, value]) => {
-    if (value.length > 0) {
-      if (key === 'price') {
-        findArgs[key] = {
-          $gte: value[0],
-          $lte: value[1],
-        };
-      } else {
-        findArgs[key] = value;
+    Object.entries(req.body.filters).forEach(([key, value]) => {
+      if (value.length > 0) {
+        if (key === 'price') {
+          findArgs[key] = {
+            $gte: value[0],
+            $lte: value[1],
+          };
+        } else {
+          findArgs[key] = value;
+        }
       }
-    }
-  });
-
-  Product.find(findArgs)
-    .select('-photo')
-    .populate('category')
-    .sort([[sortBy, order]])
-    .skip(skip)
-    .limit(limit)
-    .exec((err, data) => {
-      if (err) {
-        return res.status(400).json({
-          error: 'Products not found',
-        });
-      }
-      res.json({
-        size: data.length,
-        data,
-      });
     });
+
+    const data = await Product.find(findArgs)
+      .select('-photo')
+      .populate('category')
+      .sort([[sortBy, order]])
+      .skip(skip)
+      .limit(limit);
+
+    res.json({
+      size: data.length,
+      data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: 'Internal server error',
+    });
+  }
 };
 
 exports.photo = (req, res, next) => {
